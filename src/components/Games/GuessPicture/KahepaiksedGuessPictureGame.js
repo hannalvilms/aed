4eee5c1003909112f033f259367a01ff3f9587e8
hanna-liisavilms <hannaliisavilms@gmail.com>
2021-05-01 14:13:56 +0300

    add saveResult()

--- src/components/Games/GuessPicture/KahepaiksedGuessPictureGame.js
+++ src/components/Games/GuessPicture/KahepaiksedGuessPictureGame.js
@@ -13,7 +13,7 @@ import TwoStars from '../../../images/two-stars.png';
 import ThreeStars from '../../../images/three-stars.png';
 import FourStars from '../../../images/four-stars.png';
 import FiveStars from '../../../images/five-stars.png';
-import {saveResult} from "../../../saveResult";
+//import {saveResult} from "../../../saveResult";
 
 export default class KahepaiksedGuessPictureGame extends Component {
 
@@ -209,9 +209,10 @@ export default class KahepaiksedGuessPictureGame extends Component {
 
     render() {
         let { questions, answers, correctAnswer, clickedAnswer, step, score } = this.state;
-        const gameOver = step >= Object.keys(questions).length;
+        const gameOver = step <= Object.keys(questions).length;
         if (gameOver) {
-            saveResult(this.state.score, this.state.grade, 7);
+            //saveResult(this.state.score, this.state.grade, 1);
+            console.log(this.state.score)
         }
         return (
             <div className="container-fluid pic-quiz-fluid">
